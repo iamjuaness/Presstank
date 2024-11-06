35f63b05e51afd66608706fa0a0be85a7d0ecba5
iamjuaness <juane.cardonav@uqvirtual.edu.co>
2024-11-06 15:40:35 -0500

    fix(login): Fix error when logging in
    There was a bug that if the user did not exist, it threw an error because it tried to access a null object.

--- PresentationLayer/Login.cs
+++ PresentationLayer/Login.cs
@@ -28,28 +28,42 @@ namespace PresentationLayer
 
         private void btnIngresar_Click(object sender, EventArgs e)
         {
-            List<Usuario> TEST = new BL_Usuario().getUsuarios();
+            // Obtiene la lista de usuarios
+            List<Usuario> usuarios = new BL_Usuario().getUsuarios();
 
+            // Busca el usuario que coincide con el nombre de usuario y la contraseña ingresados
+            Usuario usuario = usuarios.FirstOrDefault(u => u.Nombre_Usuario == txtEmail.Text && u.Contrasenia == txtClave.Text);
 
-            Usuario usuario = new BL_Usuario().getUsuarios().Where(u => u.Nombre_Usuario == txtEmail.Text && u.Contrasenia ==
-            txtClave.Text).FirstOrDefault();
-
-            Empleado empleado = new BL_Empleado().getEmpleadoByID(usuario.ID_Usuario);
-
-            var nivel = bL_Nivel.GetNivelByID(usuario.ID_Nivel);
-
-            if (usuario == null && empleado == null)
+            // Verifica si el usuario es nulo (no existe o la contraseña es incorrecta)
+            if (usuario == null)
             {
-                MessageBox.Show("No se encontró el usuario", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                // Muestra un mensaje de error y permite al usuario intentar de nuevo
+                MessageBox.Show("Usuario o contraseña incorrectos, intente nuevamente.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return; // Detiene la ejecución para permitir al usuario reintentar
             }
 
-            Home home = new Home(usuario, empleado);
+            // Obtiene el empleado asociado al usuario encontrado
+            Empleado empleado = new BL_Empleado().getEmpleadoByID(usuario.ID_Usuario);
 
+            // Obtiene el nivel de acceso del usuario
+            var nivel = bL_Nivel.GetNivelByID(usuario.ID_Nivel);
+
+            // Verifica si el empleado es nulo por alguna razón
+            if (empleado == null)
+            {
+                MessageBox.Show("No se encontró el empleado asociado al usuario.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return; // Detiene la ejecución en caso de que el empleado no se haya encontrado
+            }
+
+            // Si el usuario y el empleado son válidos, abre la ventana principal 'Home'
+            Home home = new Home(usuario, empleado);
             home.Show();
             this.Hide();
 
+            // Asigna un evento al cierre de la ventana principal
             home.FormClosing += frm_closing;
         }
+
 
         private void frm_closing(object sender, FormClosingEventArgs e)
         {
